be322041661820b0f2cde30bc699333b48977f6c
zhangyifan <yifanzhang2@gmail.com>
2015-06-27 15:45:51 -0500

    Setting up the iPhone app to receive location

---
+++ Running Weather/ViewController.swift
@@ -0,0 +1,38 @@
+//
+//  ViewController.swift
+//  Running Weather
+//
+//  Created by Yifan Zhang on 6/27/15.
+//  Copyright © 2015 Yifan Zhang. All rights reserved.
+//
+
+import UIKit
+import CoreLocation
+
+class ViewController: UIViewController, CLLocationManagerDelegate {
+    
+    var locationManager = CLLocationManager()
+    
+    func locationManager(manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        
+        //print(locations)
+        
+    }
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        locationManager.delegate = self
+        locationManager.desiredAccuracy = kCLLocationAccuracyBest
+        locationManager.requestAlwaysAuthorization()
+        locationManager.startUpdatingLocation()
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
