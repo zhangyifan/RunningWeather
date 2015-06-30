300ef960c006556bb809cf76e4713435ef4fc98f
zhangyifan <yifanzhang2@gmail.com>
2015-06-30 14:59:33 -0500

    Got live current temp, humidity, wind pulling from API and displaying.

--- Running Weather WatchKit 1 Extension/TodayInterfaceController.swift
+++ Running Weather WatchKit 1 Extension/TodayInterfaceController.swift
@@ -1,5 +1,5 @@
 //
-//  InterfaceController.swift
+//  TodayInterfaceController.swift
 //  Running Weather WatchKit 1 Extension
 //
 //  Created by Yifan Zhang on 6/27/15.
@@ -26,21 +26,87 @@ class TodayInterfaceController: WKInterfaceController, CLLocationManagerDelegate
     
     @IBOutlet var nowWindLabel: WKInterfaceLabel!
     
+    let weatherAPIKey = "ef2c62731316105942e0658cb48dbbd5"
+    
     var locationManager = CLLocationManager()
     
     func locationManager(manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
         
         print(locations)
         
+        //***NEED TO STOP GETTING LOCATIONS, ONLY CURRENT ONE***
+        
+    }
+    
+    func getNowWeather(latitude: Double, longitude: Double) {
+        
+        let url = NSURL(string: "http://api.openweathermap.org/data/2.5/weather?lat=\(latitude)&lon=\(longitude)&units=imperial&APPID="+weatherAPIKey)
+        
+        let task = NSURLSession.sharedSession().dataTaskWithURL(url!) { (data, response, error) -> Void in
+            
+            if error == nil {
+                
+                //print(data)
+                
+                do {
+                    
+                    var jsonResult: NSDictionary = try NSJSONSerialization.JSONObjectWithData(data!, options: NSJSONReadingOptions.MutableContainers) as! NSDictionary
+                    
+                    print(jsonResult)
+                    
+                    //Checking that the main and wind arrays exist.  ***NEED TO FIGURE OUT WEATHER DESCRIPTION ARRAY***
+                    if let main = jsonResult["main"] as? NSDictionary, wind = jsonResult["wind"] as? NSDictionary {
+                        
+                        //Checking that the fields we need exist
+                        if let temp = main["temp"] as? NSValue, humidity = main["humidity"] as? NSValue, windSpeed = wind["speed"] as? NSValue {
+                            
+                            self.nowTemperatureLabel.setText("\(temp)")
+                            
+                            self.nowHumidityLabel.setText("\(humidity)%")
+
+                            //***NOTE NEED TO TURN WINDSPEED INTO INT TO SAVE SPACE***
+                            self.nowWindLabel.setText("\(windSpeed)")
+                            
+                        } else {
+                            
+                            print("Error with main or wind values inside")
+                            
+                        }
+                        
+                    } else {
+                        
+                        print("Error with main or wind arrays")
+                        
+                    }
+                    
+                } catch {
+                    
+                    print("Error happened with JSON")
+                    
+                }
+                
+            } else {
+                
+                print(error)
+                
+            }
+        }
+        
+        task!.resume()
+        
     }
 
     override func awakeWithContext(context: AnyObject?) {
         super.awakeWithContext(context)
         
+        getNowWeather(41.8369, longitude: -87.6847)
+        
         locationManager.delegate = self
         locationManager.desiredAccuracy = kCLLocationAccuracyBest
         locationManager.requestAlwaysAuthorization()
         locationManager.startUpdatingLocation()
+        
+        print("App launched")
     }
 
     override func willActivate() {
